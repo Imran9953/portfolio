20d4d7d6e807ddcc0fc21e6b960c84c480c9603f
Imran9953 <imraanabbass786@gmail.com>
2024-03-14 17:29:17 -0700

    portfolio

---
+++ script.js
@@ -0,0 +1,69 @@
+
+//         //  Arthimatic operation
+
+// let numFirt=10;
+// let numSecond=20;
+
+//         // Addition
+
+// let sum=numFirt+numSecond;
+// console.log(sum, "   Sum of two numbers");
+
+//         //  Subtraction
+
+// let sub= numSecond-numFirt;
+// console.log(sub, "   Subtraction of two numbers");
+
+//         //  Division
+
+// let div = numSecond-numFirt;
+// console.log(div, "   Division of two numbers");
+
+//         // Product
+
+// let prod=numFirt+numSecond;
+// console.log(prod, "   Product of two numbers");
+
+
+//          //Exponential           
+
+
+// let exp=numFirt**2;
+// console.log(exp, "  Power or numFirst");
+
+//     //  Reminder
+
+// let rem=numSecond%numFirt;
+// console.log(rem, "    Reminder of two numbers");
+
+
+
+
+
+
+//     //  Equalities operation
+
+//        // greater than
+
+// let greaterThan=numFirt>numSecond;
+// console.log(greaterThan, "Greater than");
+
+//        // Lessthan
+
+// let lessThan=numFirt < numSecond;
+//  console.log(lessThan, "lessThan");
+
+//               // Greaterthan and equal
+
+// let greaterThanEqual=numFirt >= numSecond;
+// console.log(greaterThanEqual, "Greater than and equal");
+
+//        // Less than and equal
+
+//  let lessThanEqual=numFirt <= numSecond;
+//  console.log(lessThanEqual, "Less than and equal");
+
+//         //    not equal 
+
+// let notEqual=numFirt!=numSecond;
+// console.log(notEqual,"Not equal")
